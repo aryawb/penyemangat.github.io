f4aea8d577096606098b97c5dc5b7cfea4731607
aryawb <61925684+aryawb@users.noreply.github.com>
2021-02-08 21:58:36 +0700

    Add files via upload

---
+++ klikawal.js
@@ -0,0 +1,10 @@
+
+function lala()
+{
+	swal.fire({
+		title: 'Haii Berlyan',
+		text: "Bagaimana kabarmu hari ini?",
+		icon: 'question',
+		confirmButtonText: 'Baik',
+		cancelButtonText: 'Tidak'
+	});
